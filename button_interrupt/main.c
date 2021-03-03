f9a0e86b171f5ffbb6f4c4846a1d7ae6e90d1494
silenc3502 <gcccompil3r@gmail.com>
2021-03-04 02:24:05 +0900

    Revert Button Interrupt

--- button_interrupt/main.c
+++ button_interrupt/main.c
@@ -7,14 +7,24 @@
 
 int main(void)
 {
-    DDRA = 0x01;
-    DDRC = 0xff;
-    DDRF = 0x0f;
+    DDRB &= ~(1 << DDB0);     // Clear the PB0 pin
+    // PB0 (PCINT0 pin) is now an input
 
-    PORTA = 0x01;
-    PORTC = 0x77;
-    PORTF = 0x01;
+    PORTB |= (1 << PORTB5);    // turn On the Pull-up
+    // PB0 is now an input with pull-up enabled
+
+    PCICR |= (1 << PCIE0);    // set PCIE0 to enable PCMSK0 scan
+    PCMSK0 |= (1 << PCINT0);  // set PCINT0 to trigger an interrupt on state change
+
+    sei();                    // turn on interrupts
 
     while(1)
         ;
+}
+
+ISR (PCINT0_vect)
+{
+    PORTB ^= 0x20;
+
+    //_delay_ms(500);
 }
